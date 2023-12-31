438fb5d70cfa8ff97bbb885a55a675c9698019e7
RaviVersionControl <89581636+RaviVersionControl@users.noreply.github.com>
2023-12-31 08:45:07 +0530

    Update main.tf
    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
+  security_groups = [var.security_group]
   tags= {
     Name = var.tag_name
   }
