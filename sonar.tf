1d536673f9ab0190a41230d4dc015d103c5220d8
RaviVersionControl <89581636+RaviVersionControl@users.noreply.github.com>
2024-01-07 17:26:40 +0530

    Update sonar.tf
    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "mySonarInstance" {
     }
 
  resource "aws_security_group" "sonar-sg-2022" {
+      id          = "sg-065d03c0b247132a3"
       name        = "security_sonar_group_2022"
       description = "security group for Sonar"
 
