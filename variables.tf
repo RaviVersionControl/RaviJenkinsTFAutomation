b551f4e9f705755d2733e5ba1249ba6260123325
RaviVersionControl <89581636+RaviVersionControl@users.noreply.github.com>
2024-01-07 16:49:54 +0530

    Update variables.tf
    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -38,7 +38,7 @@ variable "acl" {
 }
 variable "bucket_prefix" {
     type        = string
-    region      = var.region
+    region      = var.aws_region
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
     default     = "my-s3bucket-"
 }
